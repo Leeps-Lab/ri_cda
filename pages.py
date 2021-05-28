ffdfe592e46726f39de6d8346c9798354a7d9e47
Leqi Zeng <leqizeng00@gmail.com>
2021-05-28 09:12:37 -0700

    fix

--- pages.py
+++ pages.py
@@ -27,7 +27,7 @@ class block_page(Page):
         }
 
 class Start(Page):
-    timeout_seconds = 60
+    ##timeout_seconds = 60
     form_model = 'player'
     form_fields = ['width', 'cost', 'm_low', 'm_high', 'low_val', 'high_val']
 
